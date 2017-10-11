9f2d64e134c76a9af2e3c0beeee2f1db189a3eef
Andrew_Kuykendall <Andrew_Kuykendall@FOS-30127>
2017-10-11 11:50:19 -0500

    Add project files.

---
+++ Payment Project/Main Form.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblDiscountPrice = New System.Windows.Forms.Label()
+        Me.btnCalc = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lstRates = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(131, 54)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(101, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Original Price:"
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Location = New System.Drawing.Point(132, 78)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.Size = New System.Drawing.Size(100, 27)
+        Me.txtPrice.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(262, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(68, 20)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "&Rate (%):"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 142)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(129, 20)
+        Me.Label3.TabIndex = 6
+        Me.Label3.Text = "&Discounted Prices:"
+        '
+        'lblDiscountPrice
+        '
+        Me.lblDiscountPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDiscountPrice.Font = New System.Drawing.Font("Segoe UI Semibold", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblDiscountPrice.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.lblDiscountPrice.Location = New System.Drawing.Point(16, 162)
+        Me.lblDiscountPrice.Name = "lblDiscountPrice"
+        Me.lblDiscountPrice.Size = New System.Drawing.Size(157, 95)
+        Me.lblDiscountPrice.TabIndex = 7
+        '
+        'btnCalc
+        '
+        Me.btnCalc.Location = New System.Drawing.Point(179, 222)
+        Me.btnCalc.Name = "btnCalc"
+        Me.btnCalc.Size = New System.Drawing.Size(86, 35)
+        Me.btnCalc.TabIndex = 4
+        Me.btnCalc.Text = "&Calculate"
+        Me.btnCalc.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(271, 222)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(78, 35)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lstRates
+        '
+        Me.lstRates.FormattingEnabled = True
+        Me.lstRates.ItemHeight = 20
+        Me.lstRates.Location = New System.Drawing.Point(266, 78)
+        Me.lstRates.Name = "lstRates"
+        Me.lstRates.Size = New System.Drawing.Size(64, 84)
+        Me.lstRates.TabIndex = 3
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(366, 277)
+        Me.Controls.Add(Me.lstRates)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalc)
+        Me.Controls.Add(Me.lblDiscountPrice)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Discount Warehouse"
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lblDiscountPrice As Label
+    Friend WithEvents btnCalc As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lstRates As ListBox
+End Class
---
+++ Payment Project/Main Form.vb
@@ -0,0 +1,68 @@
+﻿' Name:         Discount Project
+' Purpose:      Display the discount amount and rates from 10% to 40%
+' Programmer:   Andrew Kuykendall on 10/11/2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' fill list box with rates and select first rate
+
+        For dblRates As Double = 10 To 40 Step 5
+            lstRates.Items.Add(dblRates.ToString("N1"))
+        Next
+
+        lstRates.SelectedItem = "10.00"
+
+    End Sub
+
+    Private Sub btnCalc_Click(sender As Object, e As EventArgs) Handles btnCalc.Click
+        ' display the monthly mortgage payment
+        Dim dblOriginalPrice As Double
+        Dim dblRate As Double
+        Dim dblDiscountPrice As Double
+        Dim dblDiscountTotal As Double
+
+        Double.TryParse(txtPrice.Text, dblOriginalPrice)
+        Double.TryParse(lstRates.SelectedItem.ToString, dblRate)
+
+        dblRate /= 100
+
+        lblDiscountPrice.Text = String.Empty
+
+        'Calculate discount
+        dblDiscountTotal = dblOriginalPrice * dblRate
+        dblDiscountPrice = dblOriginalPrice - dblDiscountTotal
+
+        'Display the Discount Information
+        lblDiscountPrice.Text = lblDiscountPrice.Text & " You saved : " & dblDiscountTotal.ToString("c2") & ControlChars.NewLine &
+            " Your Total is: " & dblDiscountPrice.ToString("C2")
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtPrincipal_Enter(sender As Object, e As EventArgs) Handles txtPrice.Enter
+        txtPrice.SelectAll()
+    End Sub
+
+    Private Sub ClearPayment(sender As Object, e As EventArgs) Handles txtPrice.TextChanged, lstRates.SelectedValueChanged
+        lblDiscountPrice.Text = String.Empty
+    End Sub
+
+    Private Sub txtPrincipal_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPrice.KeyPress
+        ' accept only numbers and the Backspace key
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+                e.KeyChar <> ControlChars.Back AndAlso e.KeyChar <> "." Then
+            e.Handled = True
+        End If
+    End Sub
+
+
+End Class
